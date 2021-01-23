3dddd39148411748a30178074aed83dc0253953a
Steven Hong <steven.hong6ec@gmail.com>
2021-01-22 22:47:23 -0500

    button functionality added to raindrop

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,16 @@
 var m = document.getElementById("player");
+var p = document.getElementById("words");
+
 function controlAudio() {
   if (m.paused) {
+    p.classList.add("words-active");
     m.play();
   } else {
+    p.classList.remove("words-active");
     m.pause();
   }
 }
+
 function setAudio(volume) {
   m.volume = volume;
 }
